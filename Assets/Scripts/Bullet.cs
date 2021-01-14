21752f3e3424dc1f4ea09d5ad43e9f6d4312bc5b
BoxyBoy <ljmathers20@gmail.com>
2021-01-14 12:59:22 -0700

    First commit to repository
    First commit of files to repository (Setup)

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Range(1, 100)] public float speed = 10.0f;
+    
+
+    public void Fire(Vector3 forward)
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(Vector3.forward * speed, ForceMode.VelocityChange);
+    }
+}
